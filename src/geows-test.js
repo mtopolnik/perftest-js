de2e37e70bb4db7df2dcebfc4f87d2a84fbb5bc7
Marko Topolnik <mtopolnik@inge-mark.hr>
2013-07-03 12:50:46 +0200

    geows-test

--- src/geows-test.js
+++ src/geows-test.js
@@ -1,32 +1,32 @@
 function logResult(r) {
-   var json = r.stringBody();
-   log.debug("Response: {}",json);
+   spy('Response: {}', r.stringBody());
 }
 
 function conf(b) {
    format = java.lang.String.format;
-   base = "http://localhost:4180/geows";
+   base = 'http://localhost:4180';
 }
 
 function init() {
-   req.acceptableStatus("ok");
-   test(); 
-}
-
-function test() {
-   req("suggest-street").get(spy("suggest URL", url(base).s("suggest","street") 
-         .q("zip","10000", "place","zagreb", "street","Heinzelo")))
+   req.acceptableStatus('ok');
+   req('suggest-street').get(spy('suggest URL', url(base).s('geows','suggest','street') 
+         .q('zip','10000', 'place','zagreb', 'street','Heinzelo')))
          .go(logResult);
-   req("validation-success").get(spy("validation URL", url(base).s("validation")
-         .q("zip","10000", "place","zagreb", "street","Heinzelova", "number","47", "subn","b")))
+   req('validation-invalid').get(spy('validation URL', url(base).s('geows','validation')
+         .q('zip','10000', 'place','zagb', 'street','Hein', 'number','47', 'subn','b')))
          .go(logResult);
-   req("validation-street").get(spy("validation URL", url(base).s("validation")
-         .q("zip","10000", "place","zagreb", "street","Heinzelova", "number","11", "subn","b")))
+   req('validation-place').get(spy('validation URL', url(base).s('geows','validation')
+         .q('zip','10000', 'place','zagreb', 'street','Hein', 'number','47', 'subn','b')))
          .go(logResult);
-   req("validation-place").get(spy("validation URL", url(base).s("validation")
-         .q("zip","10000", "place","zagreb", "street","Hein", "number","47", "subn","b")))
+   req('validation-street').get(spy('validation URL', url(base).s('geows','validation')
+         .q('zip','10000', 'place','zagreb', 'street','Heinzelova', 'number','11', 'subn','b')))
          .go(logResult);
-   req("validation-invalid").get(spy("validation URL", url(base).s("validation")
-         .q("zip","10000", "place","zagb", "street","Hein", "number","47", "subn","b")))
-         .go(logResult);
+   req('validation-success').get(spy('validation URL', url(base).s('geows','validation')
+         .q('zip','10000', 'place','zagreb', 'street','Heinzelova', 'number','47', 'subn','b')))
+         .go(function(r) {
+            logResult(r);
+            req('delivery-zones').get(spy('dz URL {}', url(base).s(
+                  'p5/address', r.jsonBody().id.toFixed(), 'delivery-zones')
+               )).go(logResult);
+         });
 }
